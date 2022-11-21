53c975929a2e2741dd11db0590e99ca5755928e9
Abhijeet_Dhawale <abhijeet.dhawale@mindstix.com>
2022-11-21 18:58:21 +0530

    first commit

---
+++ v2-vpc-module-standardlized/c2-generic-variable.tf
@@ -0,0 +1,21 @@
+# Input Variable
+ 
+# AWS Region
+variable "aws_region" {
+  description = "Region in which aws resource will be created"
+  type = string
+  default = "ap-south-1"
+}
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "dev"
+}
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "HR"
+}
+ 
